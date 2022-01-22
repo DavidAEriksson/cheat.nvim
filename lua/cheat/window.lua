80879007c4049334ef0ccc36d9e659cb63e0f737
David <david.eriksson@valtech.com>
2022-01-22 21:50:52 +0100

    Window updates

--- lua/cheat/window.lua
+++ lua/cheat/window.lua
@@ -3,8 +3,20 @@ local M = {}
 local popup = require 'plenary.popup'
 
 M.create_window = function()
-  local win_id = popup.create({ "" }, {})
-  print(win_id)
+  
+
+  local win = vim.api.nvim_open_win(0, false,{ col=3, width=12, height=3})
+  print(win)
+  -- local win_id = popup.create({ "Opt1" ,"Opt2" }, {
+  --   line = 15,
+  --   col = 45,
+  --   minwidth = 20,
+  --   border = true,
+  --   callback = function(win_id, sel)
+  --     print(sel)
+  --   end,
+  -- })
+  -- print(win_id)
 end
 
 return M
