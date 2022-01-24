0b72de5bc0216e2bbf89f6be85e2700fb9508108
David <david.eriksson@valtech.com>
2022-01-24 14:07:05 +0100

    (feat): curl. Make select file type optional, if not selected choose file type

--- lua/cheat/curl.lua
+++ lua/cheat/curl.lua
@@ -1,20 +1,24 @@
 local curl = require "plenary.curl"
+local filetype = require "plenary.filetype"
 
 local M = {}
 
 M.get_lang = function ()
-  local lang = vim.fn.input("Language: ", "")
+  local lang = vim.fn.input("[Cheat.nvim] Select language, leave blank for current file: ", "")
+  if lang == nil or lang == '' then
+    lang = filetype.detect(vim.fn.expand('%:t'))
+  end
   return lang
 end
 
 M.get_query = function ()
-  local query = vim.fn.input("Query: ", "")
+  local query = vim.fn.input("[Cheat.nvim] Type query: ", "")
   return query:gsub(" ", "+")
 end
 
 M.query = function(lang, lang_query)
   local res = curl.get("https://cheat.sh/" .. lang .. "/" .. lang_query .. "\\?QT")
-  return res
+  return res.body
 end
 
 return M
--- lua/cheat/window.lua
+++ lua/cheat/window.lua
@@ -1,9 +1,37 @@
 local curl = require('cheat.curl')
+local popup = require('plenary.popup')
 
 local M = {}
 
+M.window_opts = function ()
+  local w_width = math.floor(vim.o.columns * 0.7)
+  local w_height = math.floor(vim.o.lines * 0.7)
+
+  local w_left = math.floor((vim.o.columns - w_width) / 2)
+  local w_top = math.floor(((vim.o.lines - w_height) / 2) - 1)
+
+  local opts = {
+    relative = "editor",
+    row = w_top,
+    col = w_left,
+    width = w_width,
+    height = w_height,
+    style = "minimal",
+  }
+
+  return opts
+end
+
 M.create_window = function()
   local query_result = curl.query(curl.get_lang(), curl.get_query())
+
+  print("?", query_result)
+  -- local w_opts = M.window_opts()
+  -- local w_buf = vim.api.nvim_create_buf(false, true)
+  -- vim.api.nvim_buf_set_keymap(w_buf, "n", "q", ":bd<CR>", { noremap = true })
+  -- vim.api.nvim_buf_set_lines(w_buf, 0, -1, false, t)
+  -- local w_win = vim.api.nvim_open_win(w_buf, true, w_opts)
+
 end
 
 return M
